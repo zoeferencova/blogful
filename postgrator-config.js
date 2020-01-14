f6f54f9068f476c3cf473821484a9f2d0f1b4af1
zoeferencova <zoeferencova@gmail.com>
2020-01-13 22:06:28 -0500

    modified test articles

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    "migrationsDirectory": "migrations",
+    "migrationDirectory": "migrations",
     "driver": "pg",
-    "connectionString": (process.env.DB_URL === 'test')
-        ? process.env.TEST_DB_URL
-        : process.env.DB_URL,
-}
+    "connectionString": (process.env.NODE_ENV === 'test')
+      ? process.env.TEST_DB_URL
+      : process.env.DB_URL,
+  }
